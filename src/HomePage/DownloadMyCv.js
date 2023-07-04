285c72555de6461ee7b51842dbe67de2ca798bc9
Dima1Garbar <dima.qarbar@gmail.com>
2023-07-04 16:28:54 +0300

    Add CV

--- src/HomePage/DownloadMyCv.js
+++ src/HomePage/DownloadMyCv.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DownloadMyCv = () => {
   return (
     <div className="btnDiv">
-    <a target="_blank"  href="https://docs.google.com/document/d/1ZxMWOBlVkZI6d59uUITFr0Su2Ni9Yudm/edit?usp=sharing&ouid=109996653742047934880&rtpof=true&sd=true"  download="CV"> 
+    <a target="_blank"  href="https://docs.google.com/document/d/1yFzYHWRrh2G_B_-8c9ja632KKcZJGQl9/edit?usp=sharing&ouid=109996653742047934880&rtpof=true&sd=true"  download="CV"> 
     <button className='CV'>Dowload CV</button>
     </a>
     </div>
